8d293b75a79d94ddd130369705cd67419f3cd14c
leart-zuka-titanom <leart.zuka@titanom.com>
2022-08-28 01:13:00 +0200

    clean up filesystem

--- Python/O'Reilly: Python for Devops/Chapter 2/Automating_Files_and_the_Filesystem.py
+++
@@ -1,7 +0,0 @@
-"""
-It is better to be in a virtual environment when downloading special packages, so you don't mess up certain dependencies especially on linux
-In order to do that type:
-    $python -m venv venv      //This creates a virtual environment with the name venv
-    $source venv/bin/activate //This activates the virtual environment
-Please for the love of god don't commit this folder to GitHub
-"""
