e437e1d24dcb080550d4db615f229160db9fad29
leart-zuka-titanom <leart.zuka@titanom.com>
2022-08-28 00:55:35 +0200

    feat: add more functions and finish of chapter 1

--- Python/O'Reilly: Python for Devops/Chapter 1/Basics.py
+++ Python/O'Reilly: Python for Devops/Chapter 1/Basics.py
@@ -1,3 +1,11 @@
+""" 
+It is better to be in a virtual environment when downloading special packages so you don't mess up certain dependencies especially on linux
+In order to do that type:
+    $python -m venv venv      //This creates a virtual environment with the name venv
+    $source venv/bin/activate //This activates the virtual environment
+"""
+
+
 def HelloWorld():
     print("Hello World")
     return
@@ -46,3 +54,73 @@ def Split():
         url.split("/")
     )  # uses whitespaces by default but can also use different characters
     return
+
+
+def count():
+    n = 0
+    while True:
+        n += 1
+        yield n  # basically yield is just a fancy return, everytime our function (here specifically it is a generator) it returns the value specified by yield and then pauses its state until it is next called
+
+
+def fibonacci_generator():
+    first = 0
+    last = 1
+    while True:
+        first, last = last, first + last
+        yield first
+
+
+"""
+In the book there is an interesting part at the end of this chapter regarding the IPython interpreter and how you can use it to execute shell command.
+In order to use native python to execute shell commands, you should prolly just use the library os which comes with a lot of stuff regarding your system and how to control it with python scripts.
+"""
+
+
+def Exercise_1(name):
+    """
+    Exercise was to write a function that takes a name and prints name.
+    Could be improved by checking if name is empty or not and/or if the name is a string or not.
+    """
+    print(name)
+    return
+
+
+def Exercise_2(string):
+    """
+    Exercise was to write a function that takes a string and prints out wether it is upper or lower case.
+    Was improved by adding the case that if the name both contains upper and lower case chars that it prints out neither.
+    Could be improved by ideas mentioned in Exercise_1().
+    """
+    if string.isupper():
+        print("upper")
+        return
+    elif string.islower():
+        print("lower")
+        return
+    print("neither")
+    return
+
+
+def Exercise_3():
+    """
+    Exercise was to write a function that takes the string "smog-tether" and inserts that string into a list with the string all capitalized.
+    Could be improved by ideas mentioned in Exercise_1().
+    """
+    string = "smog-tether"
+    list = [x.upper() for x in string]
+    print(list)
+
+
+def Exercise_4():
+    """
+    Exercise was to write a generator that alternates between even and odd.
+    """
+    n = 0
+    while True:
+        if n % 2 == 0:
+            n += 1
+            yield "even"
+        else:
+            n += 1
+            yield "odd"
