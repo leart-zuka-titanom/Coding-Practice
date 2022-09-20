5687412a37ed262370ef2f719330dd378dc0a6b0
leart-zuka-titanom <leart.zuka@titanom.com>
2022-09-21 01:23:11 +0200

    feat: comments

--- Python/O'Reilly: Python for Devops/Chapter 3/say_it.py
+++ Python/O'Reilly: Python for Devops/Chapter 3/say_it.py
@@ -2,7 +2,7 @@
 
 """
 To eliminate the need to explicitly call type python on the command line when you run your script, you can add the line '#!/usr/bin/env python' to the
-top of your file
+top of your file. It tells the operating system to run the script with the Python interpreter. This is called a shebang line.
 """
 
 
@@ -17,5 +17,9 @@ if __name__ == "__main__":
     say_it()
 
 """
-Then maek the file executable and call it in a shell without directly invoking Python
+Then make the file executable and call it in a shell without directly invoking Python.
+So basically,
+1. Add the shebang line to the top of the file
+2. Make the file executable with the command 'chmod +x say_it.py'
+3. Run the script with './say_it.py'
 """
